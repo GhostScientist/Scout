ce3e35cbbb37007b3cd98d4f88caafd4b7790ec4
Dakota Kim <dakotakim@aprils-macbook-air.local>
2018-09-24 03:58:54 -0400

    add mapview, textfield

--- Scout/Search/SearchViewController.swift
+++ Scout/Search/SearchViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: - IB Outlets
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.hideKeyboardWhenTappedAround()
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.dismissKeyboard()
+        return true
+    }
+}
 
-
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
 }
 
