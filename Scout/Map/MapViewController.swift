3746aae96640e62e558e7b931f0ed046a85217c1
Dakota Kim <S0281634@SalemState.edu>
2018-09-28 23:57:11 -0400

    Add Core Location

--- Scout/Map/MapViewController.swift
+++ Scout/Map/MapViewController.swift
@@ -8,13 +8,47 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
 
+final class cityAnnotation: NSObject, MKAnnotation {
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    
+    init(coordinate: CLLocationCoordinate2D, title: String?) {
+        self.coordinate = coordinate
+        self.title = title
+        super.init()  // initialise super class
+    }
+        
+    var region: MKCoordinateRegion {
+        let span = MKCoordinateSpan(latitudeDelta: 1.0, longitudeDelta: 1.0)  // 1 degree is approx 111kms
+        return MKCoordinateRegion(center: coordinate, span: span)
+    }
+        
+    }
+
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+    
+    // Properties
+    
+    var locationManager = CLLocationManager()
+    var userLocation: CLLocation?
+    
+    // MARK: - IB Outlet
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
+        userLocation = locationManager.location
+        let currentCoordinate = CLLocationCoordinate2D(latitude: userLocation!.coordinate.latitude, longitude: userLocation!.coordinate.longitude)
+        let locationAnnotation = cityAnnotation(coordinate: currentCoordinate, title: "Current Location")
+        mapView.addAnnotation(locationAnnotation)
+        mapView.setRegion(locationAnnotation.region, animated: true)
     }
     
     var loginPresented = false
@@ -30,3 +64,17 @@ class MapViewController: UIViewController {
 
 }
 
+extension MapViewController: MKMapViewDelegate {
+    func Map(_ Map: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if let cityAnnotation = Map.dequeueReusableAnnotationView(withIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier) as? MKMarkerAnnotationView {
+            cityAnnotation.animatesWhenAdded = true
+            cityAnnotation.titleVisibility = .adaptive
+            cityAnnotation.subtitleVisibility = .adaptive
+            
+            return cityAnnotation
+            
+        }
+        
+        return nil
+    }
+}
