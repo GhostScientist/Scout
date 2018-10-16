151e3389f56b0266615b8707482593bee065da48
Dakota Kim <S0281634@SalemState.edu>
2018-10-16 12:14:32 -0400

    add description textview and post button

--- Scout/Add/AddViewController.swift
+++ Scout/Add/AddViewController.swift
@@ -11,11 +11,32 @@ import AVFoundation
 import CoreLocation
 import Firebase
 
-class AddViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class AddViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextViewDelegate {
     
     // MARK: - IB Outlets
     
     @IBOutlet weak var imageView: UIImageView!
+    
+    let descriptionTextView : UITextView = {
+        let t = UITextView()
+        t.text = ""
+        t.backgroundColor = UIColor.white
+        t.layer.cornerRadius = 10.0
+        t.layer.masksToBounds = true
+        t.font = UIFont.systemFont(ofSize: 15.0)
+        return t
+    }()
+    
+    let postButton : UIButton = {
+        let post = UIButton()
+        post.setTitle("Post", for: .normal)
+        post.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24.0)
+        post.setTitleColor(UIColor.black, for: .normal)
+        post.backgroundColor = UIColor.white
+        post.layer.masksToBounds = true
+        post.addTarget(self, action: #selector(postTapped), for: .touchUpInside)
+        return post
+    }()
     
     // MARK: - Instance Variables
     var captureSession: AVCaptureSession?
@@ -30,6 +51,7 @@ class AddViewController: UIViewController, UIImagePickerControllerDelegate, UINa
         imagePicker.delegate = self
         imageView.layer.cornerRadius = 10.0
         imageView.layer.masksToBounds = true
+        descriptionTextView.delegate = self
         imageView.image = UIImage(named: "camera")
         
         capturePhotoOuput = AVCapturePhotoOutput()
@@ -105,6 +127,8 @@ class AddViewController: UIViewController, UIImagePickerControllerDelegate, UINa
                 self.present(self.imagePicker, animated: true)
             }
         }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+        ac.addAction(cancelAction)
         ac.addAction(choosePhotoAction)
         ac.addAction(takePhotoAction)
         present(ac, animated: true)
@@ -112,7 +136,7 @@ class AddViewController: UIViewController, UIImagePickerControllerDelegate, UINa
     
     
     func setupImageView() {
-        imageView.anchors(top: view.safeAreaLayoutGuide.topAnchor, topPad: 24, bottom: nil, bottomPad: 0, left: view.leftAnchor, leftPad: 12, right: view.rightAnchor, rightPad: 12, height: 3 * (view.frame.height / 4), width: 0)
+        imageView.anchors(top: view.safeAreaLayoutGuide.topAnchor, topPad: 24, bottom: nil, bottomPad: 0, left: view.leftAnchor, leftPad: 12, right: view.rightAnchor, rightPad: 12, height: 4 * (view.frame.height / 7), width: 0)
     }
     
 
@@ -122,6 +146,9 @@ class AddViewController: UIViewController, UIImagePickerControllerDelegate, UINa
             imageView.contentMode = .scaleAspectFit
             imageView.image = chosenImage
         }
+        imageView.anchors(top: view.safeAreaLayoutGuide.topAnchor, topPad: 24, bottom: nil, bottomPad: 0, left: view.leftAnchor, leftPad: 12, right: view.rightAnchor, rightPad: 12, height:(view.frame.height / 3), width: 0)
+        view.addSubview(descriptionTextView)
+        descriptionTextView.anchors(top: imageView.bottomAnchor, topPad: 8, bottom: nil, bottomPad: 0, left: imageView.leftAnchor, leftPad: 0, right: imageView.rightAnchor, rightPad: 0, height: 80, width: 0)
         
         dismiss(animated: true, completion: nil)
     }
@@ -146,6 +173,41 @@ class AddViewController: UIViewController, UIImagePickerControllerDelegate, UINa
             print("Error logging out.")
         }
     }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        //textView.translatesAutoresizingMaskIntoConstraints = true
+        //textView.sizeToFit()
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        view.addSubview(postButton)
+        postButton.layer.cornerRadius = 10.0
+        postButton.anchors(top: descriptionTextView.bottomAnchor, topPad: 8, bottom: nil, bottomPad: 0, left: descriptionTextView.leftAnchor, leftPad: 20, right: descriptionTextView.rightAnchor, rightPad: 20, height: 50, width: 0)
+       
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        //textView.sizeToFit()
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if(text == "\n") {
+            textView.resignFirstResponder()
+            return false
+        }
+            else {let currentText = textView.text ?? ""
+            guard let stringRange = Range(range, in: currentText) else { return false }
+            
+            let changedText = currentText.replacingCharacters(in: stringRange, with: text)
+            
+            return changedText.count <= 180
+        }
+    }
+    
+    @objc func postTapped() {
+        print("Post Tapped")
+    }
+    
 }
 
 extension AddViewController : AVCapturePhotoCaptureDelegate {
